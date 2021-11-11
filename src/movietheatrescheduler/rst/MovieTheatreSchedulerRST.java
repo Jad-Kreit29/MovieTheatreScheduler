d6560d8bb0f72d3707998800392cf91464ba771d
Jad Kreit <jojo.kreit@gmail.com>
2021-11-11 09:07:25 -0500

    Added the sleep method

--- src/movietheatrescheduler/rst/MovieTheatreSchedulerRST.java
+++ src/movietheatrescheduler/rst/MovieTheatreSchedulerRST.java
@@ -61,10 +61,10 @@ public class MovieTheatreSchedulerRST {
         //Create a while loop to keep program running so that user can add multiple tickets if they want
         do {
             //Welcome the user to the theatre
-            //TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(1);
             System.out.println("Hello! Welcome to the Screenefy Movie Theatre. This is a brand new location, so we don\'t have many movies to offer right now!");
             //Present the films and the timings to the user
-            //TimeUnit.SECONDS.sleep(2);
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("\nHere's what we have! Choose a movie that you want to see (1, 2, 3)");
 
             //Run the DisplayShows method
@@ -75,6 +75,7 @@ public class MovieTheatreSchedulerRST {
             do {
                 userChoice = in.nextInt();
                 if (userChoice > 3 || userChoice < 1) {
+                    TimeUnit.SECONDS.sleep(1/2);
                     System.out.println("This movie doesn\'t exist! Pick one we have listed");
                 } else if (userChoice == 1 || userChoice == 2 || userChoice == 3) {
                     movieChoiceCheck++;
@@ -82,7 +83,7 @@ public class MovieTheatreSchedulerRST {
             } while (movieChoiceCheck != 1);
 
             in.nextLine();
-            //TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(1);
             System.out.println("\nDo you want to hear what the movie is about? (y/n)");
 
             int check1 = 0;
@@ -90,10 +91,12 @@ public class MovieTheatreSchedulerRST {
                 userExplination = in.nextLine();
                 if (userExplination.toLowerCase().startsWith("y")) {
                     check1++;
+                    TimeUnit.SECONDS.sleep(1);
                     t.DisplayDescriptions(userChoice);
                 } else if (userExplination.toLowerCase().startsWith("n")) {
                     check1++;
                 } else if (!userExplination.toLowerCase().startsWith("y") && !userExplination.toLowerCase().startsWith("n")) {
+                    TimeUnit.SECONDS.sleep(1);
                     System.out.println("Not a vaild option. Provide a Yes or No answer");
                 }
             } while (check1 != 1);
@@ -105,7 +108,7 @@ public class MovieTheatreSchedulerRST {
             } else if (userChoice == 3) {
                 movieChoice = "Depressing Film: The 3 Hour Drama: Real Life Edition";
             }
-            //TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(1);
 
             System.out.println("\nWhat time do you want to see this movie? (1, 2, 3, etc)");
 
@@ -113,13 +116,14 @@ public class MovieTheatreSchedulerRST {
                 userTime = in.nextInt();
                 movieTime = t.UserInput(userChoice, userTime);
                 if (!movieTime.toUpperCase().endsWith("M")) {
+                    TimeUnit.SECONDS.sleep(1);
                     System.out.println("Time doesn't exist! Pick a different time");
                 }
             } while (!movieTime.toUpperCase().endsWith("M"));
             System.out.println("\nTime: " + movieTime);
 
             //Show the user the availbe screening options
-            //TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(1);
             System.out.println("\nWe've got three screening options. Choose the one that you want! (1, 2, 3)");
             m.DisplayTicketType();
 
@@ -129,6 +133,7 @@ public class MovieTheatreSchedulerRST {
                 if (typeCost == 1 || typeCost == 2 || typeCost == 3) {
                     check2++;
                 } else if (typeCost > 3 || typeCost < 1) {
+                    TimeUnit.SECONDS.sleep(1);
                     System.out.println("That screening option doesn't exist. Pick one that's lised!");
                 }
             } while (check2 != 1);
@@ -143,7 +148,7 @@ public class MovieTheatreSchedulerRST {
             }
 
             //Show the user the different age group options
-            //TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(1);
             System.out.println("\nWe've got four age groups. Elders get a $1.20 off their price! Which one do you want? (1, 2, 3, 4)");
             m.DisplayTicketAge();
 
@@ -153,6 +158,7 @@ public class MovieTheatreSchedulerRST {
                 if (ageCost == 1 || ageCost == 2 || ageCost == 3 || ageCost == 4) {
                     check3++;
                 } else if (ageCost > 4 || ageCost < 1) {
+                    TimeUnit.SECONDS.sleep(1);
                     System.out.println("We don't have that age group. Pick one we have listed!");
                 }
             } while (check3 != 1);
@@ -184,6 +190,7 @@ public class MovieTheatreSchedulerRST {
             in.nextLine();
 
             //Show possible seating arrangment and have the user pick a seat for as many tickets as they will buy
+            TimeUnit.SECONDS.sleep(1);
             System.out.println("\nHere are our seating arrangement! Our theatre is able to hold 100 seats per screen. Type the Seating Row (A, B, C, D) and then the seat number (1...25)");
 
             s.DisplaySeats();
@@ -193,6 +200,7 @@ public class MovieTheatreSchedulerRST {
             ArrayList<Integer> userSeatNumber = new ArrayList<>();
 
             for (int i = 1; i <= quantity; i++) {
+                TimeUnit.SECONDS.sleep(1);
                 System.out.println("Pick the seating option for ticket " + i);
                 int check5 = 0;
                 do {
@@ -208,16 +216,18 @@ public class MovieTheatreSchedulerRST {
                     }
 
                     if (!row.equals("A") && !row.equals("B") && !row.equals("C") && !row.equals("D")) {
+                        TimeUnit.SECONDS.sleep(1);
                         System.out.println("\nThis row doesn't exist. Pick one that we have!");
                     }
                     if (seat > 25 || seat < 1) {
+                        TimeUnit.SECONDS.sleep(1);
                         System.out.println("\nThis seat doesn't exist. Pick one that we have!");
                     }
                 } while (check5 != 1);
             }
 
             //Show the user what they've picked
-            //TimeUnit.SECONDS.sleep(2);
+            TimeUnit.SECONDS.sleep(2);
             System.out.println("\nAlright! Here's what you'll be seeing:\n\n" + movieChoice + "\t" + movieTime + "\t" + "X" + quantity + " " + ageGroup + " " + screenType + ":\t$" + sumTot);
             for (int i = 0; i < userSeatsRow.size(); i++) {
                 c.CartAdder(movieChoice, movieTime, ageGroup, screenType, sum, userSeatsRow.get(i), userSeatNumber.get(i));
@@ -225,7 +235,7 @@ public class MovieTheatreSchedulerRST {
             }
 
             //Ask if the user wants to go through and purchase another ticket
-            //TimeUnit.SECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(1);
             System.out.println("\nDo you want to buy another ticket for something else? (y/n)");
 
             int check4 = 0;
@@ -237,6 +247,7 @@ public class MovieTheatreSchedulerRST {
                 } else if (repeatChoice.toLowerCase().startsWith("y")) {
                     check4++;
                 } else if (!repeatChoice.toLowerCase().startsWith("y") && !repeatChoice.toLowerCase().startsWith("n")) {
+                    TimeUnit.SECONDS.sleep(1);
                     System.out.println("\nWhat do you mean? Tell us a Yes or No answer");
                 }
             } while (check4 != 1);
@@ -249,7 +260,7 @@ public class MovieTheatreSchedulerRST {
             totalCost += z;
         }
 
-        //TimeUnit.SECONDS.sleep(1);
+        TimeUnit.SECONDS.sleep(1);
         //Ask the user to input the amount of money they have, and see if they can pay the price
         System.out.println("\nYour total is up to: $" + totalCost + " How much money are you going to pay?");
 
@@ -257,16 +268,17 @@ public class MovieTheatreSchedulerRST {
         do {
             userMoney = in.nextDouble();
             if (userMoney < sumTot) {
+                TimeUnit.SECONDS.sleep(1);
                 System.out.println("This is insufficient amount of money. Please pay the remainder of the amount required");
             } else if (userMoney >= sumTot) {
-                //TimeUnit.SECONDS.sleep(2);
+                TimeUnit.SECONDS.sleep(2);
                 System.out.println("\nTRANSACTION COMPLETE...");
                 check6++;
             }
         } while (check6 != 1);
 
         //Thank the user for coming
-        //TimeUnit.SECONDS.sleep(4);
+        TimeUnit.SECONDS.sleep(4);
         c.DisplayCart();
         System.out.println("\nThank you so much for coming! We hope you have a great experience!");
 
